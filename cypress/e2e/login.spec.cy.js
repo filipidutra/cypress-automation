529ccc604c241480e7e215d795da3819da847bad
filipidutra <filipi_dutra_@hotmail.com>
2025-02-16 23:36:50 -0300

    Added selector List for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,41 +1,33 @@
 describe('template spec', () => {
+  
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    logginButton: "button.oxd-button",
+    secionTitleTopBar: ".oxd-text--h6",
+    wrongCredentialAlert: '.oxd-alert-content'
+
+  }
+
   it('pass', () => {
     cy.visit('opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('admin123')
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
     cy.get("button.oxd-button").click() 
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')  
-    cy.get(".oxd-text--h6").contains('Dashboard')
+    cy.get(selectorsList.secionTitleTopBar).contains('Dashboard')
 
     
   })
 
   it('login-fail', () => {
     cy.visit('opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admdain')
-    cy.get("[name='password']").type('admin123')
-    cy.get("button.oxd-button").click()
-    cy.get('.oxd-alert-content')
+    cy.get(selectorsList.usernameField).type('Admdain')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.logginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert)
 
 
   })
 
-  it('missing-password-field', () => {
-    cy.visit('opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get("button.oxd-button").click()
-    cy.get('.oxd-form').contains('Required')
-
-
-  })
-
-  it('missing-Username-field', () => {
-    cy.visit('opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='password']").type('admin1234')
-    cy.get("button.oxd-button").click()
-    cy.get('.oxd-form').contains('Required')
-
-
-  })
-  
 })
